66040091accf47113c2d26314dff02e382b5032f
S4cha <sachadso@gmail.com>
2017-11-12 12:09:17 +0100

    Adds login title

--- HappyStack/HappyStack/LoginVC.swift
+++ HappyStack/HappyStack/LoginVC.swift
@@ -22,6 +22,7 @@ final class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Login"
         v.login.addTarget(self, action: #selector(login), for: .touchUpInside)
         prefillWithPreviouslyUsedEmail()
     }
